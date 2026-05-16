0e12ddd6c13fe367cb94558932cbe31feb042a7e
Alfazu4167 <alfazuddin4167@gmail.com>
2026-05-16 12:56:09 +0600

    banner btn modified

--- src/components/shared/Banner.jsx
+++ src/components/shared/Banner.jsx
@@ -2,20 +2,21 @@ import Image from 'next/image';
 import React from 'react';
 import heroImage from "@/assets/hero.jpg"
 import Link from 'next/link';
+import { FaArrowRight } from 'react-icons/fa';
 
 const Banner = () => {
     return (
-      
+
         <div className=" bg-base-200 container mx-auto pl-5 mt-8">
             <div className="flex items-center justify-between flex-col lg:flex-row-reverse ">
                 <Image src={heroImage} alt='tiles gallery' height={220
-                } width={440} className='w-fit'/>
+                } width={440} className='w-fit' />
                 <div>
                     <h1 className="mb-5 text-5xl font-bold bg-linear-to-b from-[#59cfd9] via-[#735fc1]  to-[#e037cc] bg-clip-text text-transparent">Tiles Gallery</h1>
                     <p className="mb-5 text-gray-500 text-md">
                         Discover minimalist textures and contemporary patterns designed to bring a clean, sophisticated look to every room.
                     </p>
-                    <Link href={'/all-tiles'} className="btn bg-[#47dae7]">Browse Now</Link>
+                    <Link href={'/all-tiles'} className="btn bg-[#47dae7]">Browse Now  <FaArrowRight /> </Link>
                 </div>
             </div>
         </div>
--- src/components/shared/FeaturedTiles.jsx
+++ src/components/shared/FeaturedTiles.jsx
@@ -5,13 +5,12 @@ import { FaArrowRight } from 'react-icons/fa';
 
 const FeaturedTiles = ({tiles}) => {
     return (
-        <div className='container mx-auto mt-15'>
+        <div className='container mx-auto my-15'>
             <h2 className='text-4xl font-bold text-center'>Featured Tiles</h2>
             <div className='grid lg:grid-cols-4 gap-3 mt-15'>
                 {tiles.slice(0-4).map(tile=><TilesCard key={tile.id} tile={tile}></TilesCard>)}
             </div>
-            <Link className='btn bg-[#e037cc] mt-4 text-white' href={'/all-tiles'}>Browse Now <FaArrowRight></FaArrowRight></Link>
-        </div>
+                 </div>
     );
 };
 
