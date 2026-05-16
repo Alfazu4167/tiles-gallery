275cfbd46db742b0ccd66d71e7ca34c1a155d435
Alfazu4167 <alfazuddin4167@gmail.com>
2026-05-16 12:45:36 +0600

    readme file added

--- src/app/profile/page.jsx
+++ src/app/profile/page.jsx
@@ -18,16 +18,18 @@ const ProfilePage = () => {
     //     })
     // console.log(session);
     const user = session?.user;
+ 
     return (
         <div className='px-3 py-10 shadow-md max-w-md mx-auto  flex flex-col justify-center items-center mt-8 space-y-3 bg-base-200 border border-[#47dae7] rounded-md'>
             {
-                isPending ? <ClockLoader></ClockLoader> : <><Image src={user?.image} alt={user?.name} width={100} height={100} referrerPolicy='no-referrer' className='rounded-full'></Image>
-                    <h2 className='text-2xl font-bold text-center'>{user?.name}
-                    </h2>
-                    <p className='text-gray-500 text-center '>{user?.email}</p></>
+                isPending ? <ClockLoader></ClockLoader> :
+                    user && <><Image src={user?.image} alt={user?.name} width={100} height={100} referrerPolicy='no-referrer' className='rounded-full'></Image>
+                        <h2 className='text-2xl font-bold text-center'>{user?.name}
+                        </h2>
+                        <p className='text-gray-500 text-center '>{user?.email}</p></>
 
             }
-      <UpdateUserProfile></UpdateUserProfile>
+            <UpdateUserProfile></UpdateUserProfile>
         </div>
     );
 };
